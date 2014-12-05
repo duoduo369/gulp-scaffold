58dc557f59fb58a7c95d0797c0837529bb164e17
duoduo369 <duoduo3369@gmail.com>
2014-12-05 16:38:27 +0800

    update to auto compile

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,39 @@
 var coffee = require('gulp-coffee');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var imagemin = require('gulp-imagemin');
+var sourcemaps = require('gulp-sourcemaps');
+var del = require('del');
 var gulp = require('gulp');
 var gutil = require('gulp-util');
 
-gulp.task('coffee', function() {
-  gulp.src('./src/*.coffee')
-  .pipe(coffee({bare: true}).on('error', gutil.log))
-  .pipe(gulp.dest('./public/'))
+var paths = {
+  scripts: ['./src/*.coffee'],
+};
+
+
+gulp.task('clean', function(cb) {
+  // You can use multiple globbing patterns as you would with `gulp.src`
+  del(['build'], cb);
 });
 
-gulp.task('default', function() {
-  gulp.run('coffee')
+
+gulp.task('scripts', ['clean'], function() {
+  // Minify and copy all JavaScript (except vendor scripts)
+  // with sourcemaps all the way down
+  return gulp.src(paths.scripts)
+    .pipe(sourcemaps.init())
+    .pipe(coffee({bare: true}).on('error', gutil.log))
+    .pipe(uglify())
+    .pipe(concat('all.min.js'))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('build/js'));
 });
+
+// Rerun the task when a file changes
+gulp.task('watch', function() {
+  gulp.watch(paths.scripts, ['scripts']);
+});
+
+
+gulp.task('default', ['watch', 'scripts']);
