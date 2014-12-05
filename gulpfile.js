77517fa69ebf0c91273458559e2d645ad9face1c
duoduo369 <duoduo3369@gmail.com>
2014-12-05 16:47:41 +0800

    update package

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,12 @@
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var changed = require('gulp-changed');
 var coffee = require('gulp-coffee');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var imagemin = require('gulp-imagemin');
 var sourcemaps = require('gulp-sourcemaps');
 var del = require('del');
-var gulp = require('gulp');
-var gutil = require('gulp-util');
 
 var paths = {
   scripts: ['./src/*.coffee'],
@@ -21,13 +22,15 @@ gulp.task('clean', function(cb) {
 gulp.task('scripts', ['clean'], function() {
   // Minify and copy all JavaScript (except vendor scripts)
   // with sourcemaps all the way down
+  desc = 'build/js'
   return gulp.src(paths.scripts)
+    .pipe(changed(desc))
     .pipe(sourcemaps.init())
     .pipe(coffee({bare: true}).on('error', gutil.log))
     .pipe(uglify())
     .pipe(concat('all.min.js'))
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest('build/js'));
+    .pipe(gulp.dest(desc));
 });
 
 // Rerun the task when a file changes
